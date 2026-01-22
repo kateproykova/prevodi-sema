2d6ed33146b7840dcf6b90b11b2bf2a7aa4ec670
kateproykova <131783773+kateproykova@users.noreply.github.com>
2026-01-22 20:05:56 +0200

    Revert "Use fqdn."
    This reverts commit d9dfaa72f1efbb12f8ef5334d4dd7d806cfcccff.

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   return {
     dir: {
